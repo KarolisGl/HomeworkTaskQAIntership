7b67c91496233825e71d7a112eb2e724993fb806
KarolisGl <karolis.glodenis@stud.vilniustech.lt>
2024-08-23 20:53:21 +0300

    Moved examples elsewhere, started experimenting
    cy.visit is easy

---
+++ cypress/e2e/Technical-QA-Interview-Task.cy.js
@@ -0,0 +1,12 @@
+// cypress/e2e/3-homework-assignment
+
+
+describe('Magento software testing board, test suite(?)', () => {
+  beforeEach(() => {
+    cy.visit('https://magento.softwaretestingboard.com/');
+  });
+//first time doin javascript
+  it('Navigate to mens, Hoodies and sweatshirts', () => {
+    //cy.visit('https://magento.softwaretestingboard.com/')
+  })
+})
