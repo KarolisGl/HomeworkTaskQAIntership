7368b52d88078540036e1ea45c890982632c0aeb
KarolisGl <karolis.glodenis@stud.vilniustech.lt>
2024-08-23 21:26:05 +0300

    Update Technical-QA-Interview-Task.cy.js
    contain didn't work, need more info, takin a lil break.

--- cypress/e2e/Technical-QA-Interview-Task.cy.js
+++ cypress/e2e/Technical-QA-Interview-Task.cy.js
@@ -7,6 +7,14 @@ describe('Magento software testing board, test suite(?)', () => {
   });
 //first time doin javascript
   it('Navigate to mens, Hoodies and sweatshirts', () => {
-    //cy.visit('https://magento.softwaretestingboard.com/')
+    cy.get('a[id="ui-id-5"]').trigger('mouseover'); //mouses over the Men menu
+    
+    
+    cy.get('a[id="ui-id-17"]').trigger('mouseover'); //goes into tops
+    
+    
+    cy.get('a[id="ui-id-20"]').click(); //can't find it via cy.contains somehow, weird
+
+    //cy.get('p[id="toolbar-amount"]')
   })
 })
