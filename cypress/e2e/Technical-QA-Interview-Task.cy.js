8cf0377be0ca1f4b21b2b3737c0fd222511d8276
KarolisGl <karolis.glodenis@stud.vilniustech.lt>
2024-08-25 22:32:21 +0300

    Update Technical-QA-Interview-Task.cy.js
    lil hacky, but it'll do, 200% there's a cleaner solution to split the item count, maybe regex?

--- cypress/e2e/Technical-QA-Interview-Task.cy.js
+++ cypress/e2e/Technical-QA-Interview-Task.cy.js
@@ -8,13 +8,18 @@ describe('Magento software testing board, test suite(?)', () => {
 //first time doin javascript
   it('Navigate to mens, Hoodies and sweatshirts', () => {
     cy.get('a[id="ui-id-5"]').trigger('mouseover'); //mouses over the Men menu
-    
+    //cy.log('This is a test log???????????????????????????!???????????????????????');
     
     cy.get('a[id="ui-id-17"]').trigger('mouseover'); //goes into tops
     
     
     cy.get('a[id="ui-id-20"]').click(); //can't find it via cy.contains somehow, weird
 
-    //cy.get('p[id="toolbar-amount"]')
-  })
-})
+    cy.get('#toolbar-amount').invoke('text').then((text) => {
+      const itemCount = text.split(' ')[1];
+      const realItemCount = parseInt(itemCount.split('-')[1]);
+      cy.log('Item count: ' + realItemCount);
+      cy.get('li.item.product.product-item').should('have.length',realItemCount);
+    });
+  });
+});
